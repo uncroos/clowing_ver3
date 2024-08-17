0983222d825fbe575d724d4a3ec85928a3037641
UNCROOS <any492266@gmail.com>
2024-08-17 23:50:25 +0900

    a

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,18 @@ import 'package:clowing_ver3/screens/ootd/ootd_main.dart';
 import 'package:clowing_ver3/screens/splash/splash.dart';
 import 'package:clowing_ver3/screens/washing/washing_main.dart';
 import 'package:flutter/material.dart';
+import 'package:kakao_flutter_sdk/kakao_flutter_sdk_template.dart';
 // Import the splash screen
 
 void main() {
+  // 웹 환경에서 카카오 로그인을 정상적으로 완료하려면 runApp() 호출 전 아래 메서드 호출 필요
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // runApp() 호출 전 Flutter SDK 초기화
+  KakaoSdk.init(
+    nativeAppKey: 'e47a16bbc2de7f9bb684384b295bcd3c',
+    javaScriptAppKey: '0630e9c5a052ae423cfc0c573f8ed152',
+  );
   runApp(MyApp());
 }
 
