4a6809fdfd9a9dd28cdee2afa9b3d909dcc705b9
UNCROOS <any492266@gmail.com>
2024-08-21 22:24:09 +0900

    ㄴ

--- lib/screens/closet/clothes/top_screen.dart
+++ lib/screens/closet/clothes/top_screen.dart
@@ -5,7 +5,7 @@ import 'package:clowing_ver3/screens/closet/clothes/low_screen.dart';
 import 'package:clowing_ver3/screens/closet/clothes/outer_screen.dart';
 import 'package:clowing_ver3/screens/closet/clothes/shoes_screen.dart';
 import 'package:clowing_ver3/widgets/bottom_nav_bar.dart';
-import 'package:flutter/material.dart'; // Import LowScreen
+import 'package:flutter/material.dart';
 
 class TopScreen extends StatefulWidget {
   @override
@@ -26,109 +26,112 @@ class _TopScreenState extends State<TopScreen> {
         automaticallyImplyLeading: false,
       ),
       backgroundColor: Colors.white,
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              height: 50.0,
-              decoration: BoxDecoration(
-                color: const Color(0xFFF1E5DB),
-                borderRadius: BorderRadius.circular(8.0),
-              ),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                      child: TextField(
-                        controller: _searchController,
-                        decoration: const InputDecoration(
-                          border: InputBorder.none,
-                          hintText: '검색어 입력',
-                          hintStyle: TextStyle(color: Colors.black45),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: 50.0,
+                decoration: BoxDecoration(
+                  color: const Color(0xFFF1E5DB),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                        child: TextField(
+                          controller: _searchController,
+                          decoration: const InputDecoration(
+                            border: InputBorder.none,
+                            hintText: '검색어 입력',
+                            hintStyle: TextStyle(color: Colors.black45),
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  const Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 16.0),
-                    child: Icon(Icons.search, color: Colors.grey),
-                  ),
-                ],
+                    const Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 16.0),
+                      child: Icon(Icons.search, color: Colors.grey),
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Container(
-              width: 90,
-              height: 500,
-              padding: const EdgeInsets.only(left: 0),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Container(
+                width: 90,
+                padding: const EdgeInsets.only(left: 0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    _buildSideMenuItem('상의', true),
+                    _buildSideMenuItem('하의', false),
+                    _buildSideMenuItem('아우터', false),
+                    _buildSideMenuItem('신발', false),
+                    _buildSideMenuItem('가방', false),
+                    _buildSideMenuItem('패션 소품', false),
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20, // Reduce size for better responsiveness
+            ),
+            Container(
+              height: MediaQuery.of(context).size.height *
+                  0.6, // Set dynamic height
+              child: Row(
                 children: [
-                  _buildSideMenuItem('상의', true),
-                  _buildSideMenuItem('하의', false),
-                  _buildSideMenuItem('아우터', false),
-                  _buildSideMenuItem('신발', false),
-                  _buildSideMenuItem('가방', false),
-                  _buildSideMenuItem('패션 소품', false),
-                ],
-              ),
-            ),
-          ),
-          const SizedBox(
-            height: 50,
-          ),
-          Expanded(
-            child: Row(
-              children: [
-                VerticalDivider(thickness: 1, width: 1),
-                Expanded(
-                  child: Container(
-                    color: Colors.white,
-                    child: Column(
-                      children: [
-                        Expanded(
-                          child: GridView.count(
-                            crossAxisCount: 2,
-                            children: [],
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: ElevatedButton(
-                            onPressed: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => AddScreen()),
-                              );
-                            },
-                            child: Text('상의 추가하기'),
-                            style: ElevatedButton.styleFrom(
-                              foregroundColor: Colors.white,
-                              backgroundColor: Colors.brown[200],
-                              textStyle: TextStyle(
-                                  fontWeight: FontWeight.w900, fontSize: 17),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(8.0),
-                              ),
-                              minimumSize: Size(double.infinity, 50),
+                  VerticalDivider(thickness: 1, width: 1),
+                  Expanded(
+                    child: Container(
+                      color: Colors.white,
+                      child: Column(
+                        children: [
+                          Expanded(
+                            child: GridView.count(
+                              crossAxisCount: 2,
+                              children: [],
                             ),
                           ),
-                        ),
-                      ],
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: ElevatedButton(
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => AddScreen()),
+                                );
+                              },
+                              child: Text('상의 추가하기'),
+                              style: ElevatedButton.styleFrom(
+                                foregroundColor: Colors.white,
+                                backgroundColor: Colors.brown[200],
+                                textStyle: TextStyle(
+                                    fontWeight: FontWeight.w900, fontSize: 17),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(8.0),
+                                ),
+                                minimumSize: Size(double.infinity, 50),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
       bottomNavigationBar: BottomNavBar(),
     );
@@ -138,7 +141,8 @@ class _TopScreenState extends State<TopScreen> {
     return Container(
       color: isSelected ? const Color(0xFFEBEBEB) : Colors.white,
       width: double.infinity,
-      padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 8.0),
+      padding: const EdgeInsets.symmetric(
+          vertical: 12.0, horizontal: 8.0), // Adjust padding for responsiveness
       child: Align(
         alignment: Alignment.centerLeft,
         child: TextButton(
@@ -195,7 +199,6 @@ class _TopScreenState extends State<TopScreen> {
           MaterialPageRoute(builder: (context) => FashionScreen()),
         );
         break;
-      // Add more cases for other items if needed
     }
   }
 }
