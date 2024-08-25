6f25702f1b3969cf09acd7bd0ef55228d955da29
UNCROOS <any492266@gmail.com>
2024-08-25 18:06:08 +0900

    ㄴㅇㄹ

--- lib/screens/closet/fin/finish_screen.dart
+++ lib/screens/closet/fin/finish_screen.dart
@@ -14,10 +14,8 @@ class _FinishScreenState extends State<FinishScreen> {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Padding(
-            padding: const EdgeInsets.only(
-                top: 350.0), // Adjust this value to position the button
-          ),
+          Spacer(),
+          Spacer(),
           Image.asset(
             'assets/images/closet_pin.png', // Replace with your logo image asset path
             width: 180,
