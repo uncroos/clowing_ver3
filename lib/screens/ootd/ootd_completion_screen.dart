6854185a644bc88b14bf51090f92484330fe44a0
uncroos <any492266@gmail.com>
2024-09-15 14:04:13 +0900

    s

--- lib/screens/ootd/ootd_completion_screen.dart
+++ lib/screens/ootd/ootd_completion_screen.dart
@@ -1,5 +1,5 @@
 import 'package:clowing_ver3/screens/ootd/lookbook_completion_screen.dart';
-import 'package:clowing_ver3/screens/ootd/ootd_main.dart';
+
 import 'package:flutter/material.dart';
 
 class OotdCompletionScreen extends StatefulWidget {
